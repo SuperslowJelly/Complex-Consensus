eed82fec21ff6a5b7b1221b031ed87f8549a27a2
Chae Taylor <chaetaylor@msn.com>
2021-05-06 23:05:46 +0100

    More format adjustment.

--- src/main/java/io/github/superslowjelly/consensus/Commands.java
+++ src/main/java/io/github/superslowjelly/consensus/Commands.java
@@ -131,7 +131,7 @@ public class Commands {
         if (Consensus.instance.config.weather.minPlayers != 0 && size < Consensus.instance.config.time.minPlayers) {
             throw new CommandException(Text.of("Cannot vote to change the weather; not enough players online (required: ", Consensus.instance.config.weather.minPlayers, ")!"));
         }
-        Consensus.instance.startBooleanVote(src, "change the weather to " + weather.getName() + " in world '" + world.getName() + "'", i -> {
+        Consensus.instance.startBooleanVote(src, "change the weather to " + weather.getName().replace('_', ' ') + " in world '" + world.getName() + "'", i -> {
             if (Consensus.instance.config.weather.majority * (double) size <= i) {
                 world.setWeather(weather);
                 return true;
